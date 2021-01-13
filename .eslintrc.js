0c890747ef3e9a3ca32b80ffb2b48f8ea97e6a93
zeeshan_akram <zeeshan.akram@invozone.com>
2021-01-13 17:24:03 +0500

    assignment finished

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
+    'func-style': ['error', 'expression', { allowArrowFunctions: true }],
+  },
+};
---
+++ osInfo.js
@@ -0,0 +1,42 @@
+const dotEnv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+dotEnv.config();
+const dirPath = path.join(os.homedir(), process.env.FILENAME);
+const writeFile = async (filename, data) => {
+  try {
+    await fs.open(filename, 'r', (err) => {
+      if (err) {
+        fs.writeFile(filename, data, (error) => {
+          if (error) {
+            console.log('err', error);
+          } else {
+            console.log('file created successfully');
+          }
+        });
+      } else {
+        console.log('file aleardy exist');
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const systemInfo = {
+  Platform: os.platform(),
+  Hostname: os.hostname(),
+  OperatingSystem: os.release(),
+  UserInfo: os.userInfo(),
+  network: os.networkInterfaces(),
+};
+writeFile(dirPath, JSON.stringify(systemInfo));
+
+// todo
+// check if file exist print file does not exist
+// if file exist print file found
+// write file in a promise or async manner
+// to verify asnyc behavior print a new file has been written
+// file path set dynamically
+// make sure to remove extra file
